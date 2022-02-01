ff038be648ad833cb33306a20283ddc285b20b61
Srinivas Chintakindhi <srinivas.chkdhi@gmail.com>
2022-02-01 20:20:52 +0530

    Update stored-procedures.js

--- stored-procedures.js
+++ stored-procedures.js
@@ -1,2 +1,21 @@
 db.system.js.find()
 db.system.js.distinct("_id");
+
+
+db.system.js.save({_id: "isInRange", value: function(scrape,start,end){
+    
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    return (scrape >= startDate && scrape <= endDate);
+}});
+
+
+
+db.scrapeReoncileStatus.find({$where: "isInRange(this.scrapeTime,'2022-01-31', '2022-02-01') == true"});
+
+db.scrapeReoncileStatus.find({$where: "isInRange(this.scrapeTime,'2022-01-31', '2022-02-01') == true"});
+
+
+
+db.scrapeReoncileStatus.find({refNum:'WORKUS'})
